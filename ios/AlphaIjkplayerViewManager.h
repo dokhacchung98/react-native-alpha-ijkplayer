3dbebab1f1e60f37971acc5b84fc4a2c14857083
khacchung98 <ochung3@gmail.com>
2022-03-09 17:50:49 +0700

    fix run ios

---
+++ ios/AlphaIjkplayerViewManager.h
@@ -0,0 +1,17 @@
+//
+//  AlphaIjkplayerViewManager.h
+//  AlphaIjkplayer
+//
+//  Created by Do Chung on 09/03/2022.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AlphaIjkplayerViewManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/AlphaIjkplayerViewManager.m
@@ -0,0 +1,9 @@
+//
+//  AlphaIjkplayerViewManager.m
+//  AlphaIjkplayer
+//
+//  Created by Do Chung on 09/03/2022.
+//  Copyright © 2022 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
